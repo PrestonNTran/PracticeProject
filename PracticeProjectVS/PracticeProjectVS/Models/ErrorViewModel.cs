1414ee145b3f680fcd57162b0d33b68f7f28e723
Preston Tran <Preston.n.tran410@gmail.com>
2021-06-12 16:11:29 -0500

    VS Project Create

---
+++ PracticeProjectVS/PracticeProjectVS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PracticeProjectVS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
